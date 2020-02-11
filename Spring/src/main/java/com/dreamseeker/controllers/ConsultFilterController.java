2a8a71f4d558a5ba9897eaad9956c811a2009d29
Dreamseeker5 <edgardhernandezp@gmail.com>
2020-02-10 21:21:58 -0300

    Implemented the consult window's opening

---
+++ Spring/src/main/java/com/dreamseeker/controllers/ConsultFilterController.java
@@ -0,0 +1,11 @@
+package com.dreamseeker.controllers;
+
+import org.springframework.stereotype.Component;
+
+import net.rgielen.fxweaver.core.FxmlView;
+
+@Component
+@FxmlView("ConsultingFilters.fxml")
+public class ConsultFilterController {
+
+}
--- Spring/src/main/java/com/dreamseeker/controllers/MainController.java
+++ Spring/src/main/java/com/dreamseeker/controllers/MainController.java
@@ -1,10 +1,27 @@
 package com.dreamseeker.controllers;
 
+import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@SuppressWarnings("restriction")
 @Component
 @FxmlView("CondoAdmin.fxml")
 public class MainController {
 	
+	@Autowired
+	FxWeaver fxWeaver;
+	
+	public void openConsultWindow() {
+		Parent root = fxWeaver.loadView(ConsultFilterController.class);
+	    Scene scene = new Scene(root);
+	    Stage stage = new Stage();
+	    stage.setScene(scene);
+	    stage.show();
+	}
 }
